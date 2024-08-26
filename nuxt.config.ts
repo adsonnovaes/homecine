8e730990ca3a47d35fccabe6f2d73586c78f4ed0
Adson Novaes <adsonnovaes7@gmail.com>
2024-08-26 20:22:58 -0300

    chore (cofigs): color, package and extension settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,29 @@
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+
+  build: {
+    transpile: ["vuetify"],
+  },
+
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }));
+      });
+    },
+    "@pinia/nuxt",
+    "@nuxtjs/tailwindcss",
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+});
